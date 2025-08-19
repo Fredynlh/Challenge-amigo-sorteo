599a35f2e88c80fbfd0265ee29d13e84eff8dd0d
Fredynlh <156869313+Fredynlh@users.noreply.github.com>
2025-08-19 09:47:44 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+
+let amigos = []
+
+  function limpiarNombre(){
+    document.querySelector(".input-name").value=''
+    return;
+    
+
+
+  }
+
+
+
+
+
+
+
+function agregarAmigo() { 
+    let name = document.querySelector(".input-name").value;
+
+
+    if(name === ""){alert("Porfavor Ingrese un Nombre")}
+
+
+
+    
+   
+    else{
+        amigos.push(name)
+        document.querySelector(".section-title").innerHTML +=` <br>${name}`;
+
+       console.log(name);
+       console.log(amigos)
+        limpiarNombre();
+       
+    
+}}
+
+
+
+function sortearAmigo(){
+    if(amigos.length === 0){alert("Eres un hacker, ya que si no agregaste nada te hubieras quedado en la funcion pasada")}
+    else{
+
+        let posicionAleatoria =   Math.floor(Math.random()*amigos.length)
+       let amigoSorteado = amigos[posicionAleatoria]
+       
+        console.log(amigoSorteado)
+ document.querySelector(".section-title").innerHTML +=`<br>EL migo Ganador es <br>${amigoSorteado}`;
+    }
+
+}
+
